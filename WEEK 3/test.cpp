4e51885820e4ca184090b05f3a64caa9445fe9cd
Big Daddy <76658438+BigDaddy3898@users.noreply.github.com>
2021-03-12 00:45:24 +0700

    Add files via upload

---
+++ WEEK 3/test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main()
+{int i = -3;
+           	for( ; ; ) {
+	                	std::cout<<"Hello, world!\n";
+                    	i ++;
+	                	if (i == 0) break;
+           	} ;
+
+
+	return 0;
+}
