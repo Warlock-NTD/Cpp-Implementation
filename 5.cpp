4ace120b373a285c263b4fa285af9f3e2f26f456
Stahl <76658438+StahlDev@users.noreply.github.com>
2021-04-16 00:38:37 +0700

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+char* weird_string() {
+ char c[] = “123345”;
+ return c;
+}
+int main(int argc, char* argv[])
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    weird_string();
+    return 0;
+}
+
